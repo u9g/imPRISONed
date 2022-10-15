c819646bf419ad7d25ec3223f615834d15162ddd
U9G <git@u9g.dev>
2022-10-15 12:58:22 -0400

    Simplify troll logic

--- src/main/java/dev/u9g/imprisoned/mixin/we_do_a_bit_of_trolling/RenderLivingEntityMixin.java
+++ src/main/java/dev/u9g/imprisoned/mixin/we_do_a_bit_of_trolling/RenderLivingEntityMixin.java
@@ -1,27 +1,23 @@
 package dev.u9g.imprisoned.mixin.we_do_a_bit_of_trolling;
 
-import net.minecraft.client.Minecraft;
+import net.minecraft.client.renderer.GlStateManager;
 import net.minecraft.client.renderer.entity.RendererLivingEntity;
+import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.entity.player.EnumPlayerModelParts;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.ModifyVariable;
-import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(RendererLivingEntity.class)
 public class RenderLivingEntityMixin {
-    private boolean isCoolPerson;
-    @Redirect(method = "rotateCorpse(Lnet/minecraft/entity/EntityLivingBase;FFF)V", at = @At(value = "INVOKE", target = "Ljava/lang/String;equals(Ljava/lang/Object;)Z", ordinal = 0))
-    public boolean equals(String s) {
-        return (isCoolPerson = s.equals("U9G") || s.equals("Dinnerbone"));
-    }
-
-    @Redirect(method = "rotateCorpse", at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/player/EntityPlayer;isWearing(Lnet/minecraft/entity/player/EnumPlayerModelParts;)Z", ordinal = 0))
-    boolean isWearing(EntityPlayer instance, EnumPlayerModelParts p_175148_1_) {
-        if (isCoolPerson) {
-            return !instance.isWearing(p_175148_1_);
+    @Inject(method = "rotateCorpse", at = @At("TAIL"))
+    void onRotateCorpse(EntityLivingBase ent, float p_77043_2_, float p_77043_3_, float partialTicks, CallbackInfo ci) {
+        if (ent.deathTime > 0) return;
+        if (ent instanceof EntityPlayer && ent.getName().equals("U9G") && ((EntityPlayer)ent).isWearing(EnumPlayerModelParts.CAPE)) {
+            GlStateManager.translate(0.0f, ent.height + 0.1f, 0.0f);
+            GlStateManager.rotate(180.0f, 0.0f, 0.0f, 1.0f);
         }
-        return instance.isWearing(p_175148_1_);
     }
 }
